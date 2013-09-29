3a68ae4e51370a80bde8303cdbbedc969ce85068
twadzins <mybulkmail1212@gmail.com>
2013-09-29 12:16:16 -0500

    Initial commit of the TestBench enhancements and a few example tests., part 3 (try to fix LF issue)

--- src/test/java/org/vaadin/testbenchsauce/RallyTestCase.java
+++ src/test/java/org/vaadin/testbenchsauce/RallyTestCase.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RallyTestCase {
-    String[] value(); 
+    String[] value();
 }
